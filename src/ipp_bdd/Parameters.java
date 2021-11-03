394f6e8e588a4204d2749d98ce3cef1f723315ee
DavidAdmete <74240129+DavidAdmete@users.noreply.github.com>
2021-11-03 16:03:53 +0100

    parameters

--- src/ipp_bdd/Parameters.java
+++ src/ipp_bdd/Parameters.java
@@ -24,7 +24,64 @@ public class Parameters {
 		typMap.put("P_SIZE_0", "int");
 		typMap.put("P_COMMENT_0","string");
 		
+		typMap.put("S_SUPPKEY_0","int");
+		typMap.put("S_NAME_0","string");
+		typMap.put("S_ADDRESS_0","string");
+		typMap.put("S_NATIONKEY_0","int");
+		typMap.put("S_PHONE_0","string");
+		typMap.put("S_ACCTBAL_0","float");
+		typMap.put("S_COMMENT_0","string");
 		
+		typMap.put("PS_PARTKEY_0","int");
+		typMap.put("PS_SUPPKEY_0","int");
+		typMap.put("PS_AVAILQTY_0","int");
+		typMap.put("PS_SUPPLYCOST_0","float");
+		typMap.put("PS_COMMENT_0","string");
+		
+		typMap.put("C_CUSTKEY_0","int");
+		typMap.put("C_NAME_0","string");
+		typMap.put("C_ADDRESS_0","string");
+		typMap.put("C_NATIONKEY_0","int");
+		typMap.put("C_PHONE_0","string");
+		typMap.put("C_ACCTBAL_0","float");
+		typMap.put("C_MKTSEGMENT_0","string");
+		typMap.put("C_COMMENT_0","string");
+		
+		typMap.put("O_ORDERKEY_0","int");
+		typMap.put("O_CUSTKEY_0","int");
+		typMap.put("O_ORDERSTATUS_0","string");
+		typMap.put("O_TOTALPRICE_0","float");
+		typMap.put("O_ORDERDATE_0","string");
+		typMap.put("O_ORDERPRIORITY_0","string");
+		typMap.put("O_CLERK_0","string");
+		typMap.put("O_SHIPPRIORITY_0","int");
+		typMap.put("O_COMMENT_0","string");
+		
+		typMap.put("L_ORDERKEY_0","int");
+		typMap.put("L_PARTKEY_0","int");
+		typMap.put("L_SUPPKEY_0","int");
+		typMap.put("L_LINENUMBER_0","int");
+		typMap.put("L_QUANTITY_0","float");
+		typMap.put("L_EXTENDEDPRICE_0","float");
+		typMap.put("L_DISCOUNT_0","float");
+		typMap.put("L_TAX_0","float");
+		typMap.put("L_RETURNFLAG_0","string");
+		typMap.put("L_LINESTATUS_0","string");
+		typMap.put("L_SHIPDATE_0","string");
+		typMap.put("L_COMMITDATE_0","string");
+		typMap.put("L_RECEIPTDATE_0","string");
+		typMap.put("L_SHIPINSTRUCT_0","string");
+		typMap.put("L_SHIPMODE_0","string");
+		typMap.put("L_COMMENT_0","string");
+		
+		typMap.put("N_NATIONKEY_0","int");
+		typMap.put("N_NAME_0","string");
+		typMap.put("N_REGIONKEY_0","int");
+		typMap.put("N_COMMENT_0","string");
+		
+		typMap.put("R_REGIONKEY_0","int");
+		typMap.put("R_NAME_0","string");
+		typMap.put("R_COMMENT_0","string");
 		
 		typeMap=Collections.unmodifiableMap(typMap);
 	
